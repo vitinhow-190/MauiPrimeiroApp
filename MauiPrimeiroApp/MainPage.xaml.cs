7926f26864536c7352fdb930fb9b9de07699f00f
Usuario <Usuario@CPD03-M16>
2024-02-19 15:07:49 -0300

    Adicionar arquivos de projeto.

---
+++ MauiPrimeiroApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiPrimeiroApp
+{
+    public partial class MainPage : ContentPage
+    {
+     
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
